d4f517006519a3777d562cb4989fcf9e1cb75073
Ikumi-Otani <matelediter@gmail.com>
2016-09-02 00:32:36 +0900

    fix return indents

--- outTemp.py
+++ outTemp.py
@@ -86,10 +86,10 @@ class outTemp(OpenRTM_aist.DataFlowComponentBase):
 	#
 	def onInitialize(self):
 		# Bind variables and configuration variable
-    print "onInitialize"
-    print
+            print "onInitialize"
+            print
 		# Set InPort buffers
-		self.addInPort("origin_Temp",self._origin_TempIn)
+            self.addInPort("origin_Temp",self._origin_TempIn)
 
 		# Set OutPort buffers
 
@@ -97,8 +97,7 @@ class outTemp(OpenRTM_aist.DataFlowComponentBase):
 		# Set service consumers to Ports
 
 		# Set CORBA Service Ports
-
-		return RTC.RTC_OK
+            return RTC.RTC_OK
 
 	#	##
 	#	#
